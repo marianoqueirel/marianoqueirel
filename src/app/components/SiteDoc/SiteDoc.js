c65ec74fbb05f968e07bd45de6b088e234cfcdb0
Mariano Queirel <mariano.queirel@intive-fdv.com>
2020-05-07 16:45:45 -0300

    letting know visitros that we are under creation :)

--- src/app/components/SiteDoc/SiteDoc.js
+++ src/app/components/SiteDoc/SiteDoc.js
@@ -5,6 +5,7 @@ import Typography from "@material-ui/core/Typography";
 const Resume = () => (
   <Container maxWidth="md" style={{ textAlign: "center", paddingTop: "1rem" }}>
     <Typography variant="h2">Site Doc</Typography>
+    <Typography variant="h3">Soon...</Typography>
   </Container>
 );
 
